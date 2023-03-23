4c6de59f48d9f5d1335c72a2ad0f732acacbf5c4
Jonathan Lo <126200054+jolo988@users.noreply.github.com>
2023-03-22 22:32:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+import './style.css'
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  //define callback that runs when submit event takes place and gives access to event data
+  //use async funct to use await keyword
+  //e.preventdefault prevents whole page from refreshing when form's submitted 
+  
+  showSpinner();
+  //loading spinner
+
+  const data = new FormData(form);
+  //data obj to extract data from form (instantiate form data obj using form as input)
+
+  const response = await fetch('http://localhost:8080/dream', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt'),
+    }),
+  });
+  //request to API -> returning a promise
+  //OpenAI is a JSON API -> Set header with content type of JSON
+  //define body of request w/ form data(wrap in JSON stringify to convert to string
+  //-> then definte obj w/ value of 'prompt' -> grab prompt value from the form using GET method
+  //-> requesting data from API to give response w/ the data collected (including image URL)
+
+  if (response.ok) {
+    //ok = no errors
+
+    const { image } = await response.json();
+    //access the image by awaiting reponse.json
+
+    const result = document.querySelector('#result');
+    result.innerHTML = `<img src="${image}" width="512" />`;
+    //insert image into UI (queryselector grabs the result div and HTML)
+    //modify innerHTML w/ image tag -> where src is img URL 
+  } else {
+    const err = await response.text();
+    alert(err);
+    console.error(err);
+    //if error -> wait for response & log error
+  }
+
+  hideSpinner();
+  //after returning response -> hide spinner
+});
+
+function showSpinner() {
+  const button = document.querySelector('button');
+  button.disabled = true;
+  button.innerHTML = 'Dreaming... <span class="spinner">🧠</span>';
+}
+//grab button in DOM, set state disabled to prevent user from clicking when loading
+
+function hideSpinner() {
+  const button = document.querySelector('button');
+  button.disabled = false;
+  button.innerHTML = 'Dream'
+}
+//grabs same button, but sets disabled state false -> resets innerhtml to dream text
+
---
+++ server.js
@@ -0,0 +1,55 @@
+import * as dotenv from 'dotenv';
+dotenv.config() // access env variables that we defined in the .env file
+
+import { Configuration, OpenAIApi } from 'openai'; // use env variables when initializing openai stk (systems tool kit)
+
+const configuration = new Configuration({//config obj requiring API key
+	apiKey: process.env.OPENAI,//access API key
+});
+
+const openai = new OpenAIApi(configuration); //use config obj to initialize openai STK
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors()); //apply middleware cors (security mechanism)
+app.use(express.json()); //handle incoming data in JSON format
+
+app.post('/dream', async (req, res) => {
+	try {
+		const prompt = req.body.prompt;
+		const aiResponse = await openai.createImage({
+			prompt, 
+			n: 1,
+			size: '1024x1024',
+		});
+
+		const image = aiResponse.data.data[0].url;
+		res.send({ image });
+	} catch (error) {
+		console.error(error)
+		res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+	}
+}); 
+
+//error handling: wrap in try catch block 
+//create end-point: HTTP method POST is best option: creating new piece of data
+//catch fail on server side. error? gives error response from openAI, or else something else went wrong
+
+//takes 2 args (1st arg: string that represents URL of API. ex: localhost:8080/dream)
+//2nd arg: callback function (w/ request & response obj to interact with)
+//callback will be called everytime user navigates to this URL using async funct
+
+//prompt: access text prompt for description of image that user will generate
+//pass prompt data to openai API by calling createImage method by passing prompt as arg
+//also passing prompts with the number/resolution of image to generate (n, size)
+//await: pauses execution of function till openai finishes generating image
+
+//image: createImage will give response object containing image url
+//once image is available -> send back to user as a response for browser to receive data as JSON
+
+
+app.listen(8080, () => console.log('make art on http://localhost:8080/dream'));
+//start server with app you want to use, serve on localhost:8080
+
